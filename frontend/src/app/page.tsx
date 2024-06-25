7274adf1649fcd0145ff9fa0966d618fdc0f12c5
belelemllukas <lukasbelemel@gmail.com>
2024-06-25 20:57:03 -0300

    atualizacao

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,24 +1,14 @@
-'use client';
 import ECommerce from "@/components/Dashboard/E-commerce";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
-import RootLayout from "./layout";
-import { useRouter } from 'next/navigation';
-import { useAuth } from '@/providers/NextAuthContext';
+import { useRouter } from "next/router";
 
 export default function Dashboard() {
-  const router = useRouter();
-  const { user } = useAuth();
-  /*if (!user) { 
-    router.push('/auth/signin');
-   // return null;
-  }*/
+ 
   return (
     <>
-    
       <DefaultLayout>
         <ECommerce />
        </DefaultLayout>
-      
     </>
   );
 }
